6cd77ee6d01f4d29d15154d256eb7e236d116ae6
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-25 00:37:17 +0530

    adding statements in main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import student.Student;
 public class Main {
     public static void main(String[] args) {
         Student student = new Student(12, "Honey", "Graduate", "hmm", "Success");
-        System.out.println(student.getName());
+        System.out.println(student);
     }
 
 }
