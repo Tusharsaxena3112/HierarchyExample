72a731bdb3ec4651c3c5674223db1ab114962697
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-24 23:45:37 +0530

    adding methods

--- src/human/Human.java
+++ src/human/Human.java
@@ -7,8 +7,28 @@ public class Human {
     private String nose;
     private String hands;
 
-    public Human() {
+    public String getEyes() {
+        return eyes;
+    }
+
+    public String getLegs() {
+        return legs;
+    }
+
+    public String getHairs() {
+        return hairs;
+    }
 
 
+    public String getNose() {
+        return nose;
+    }
+
+    public String getHands() {
+        return hands;
+    }
+
+    public String toString() {
+        return String.format("I am a Human . I have some properties which are inherited by my other subclasses ");
     }
 }
