8e324b26dff2d31b28589fb1bfedab5cd21cb19d
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-25 00:14:23 +0530

    adding properties of student

--- src/student/Student.java
+++ src/student/Student.java
@@ -3,8 +3,42 @@ package student;
 import human.Human;
 
 public class Student extends Human {
+    private int age;
+    private String name;
+    private String Edu;
+    private String AofI;
+    private String aim;
 
-    Student student = new Student();
+    public Student(int age, String name, String Edu, String AofI, String aim) {
+        this.age = age;
+        this.aim = aim;
+        this.AofI = AofI;
+        this.Edu = Edu;
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEdu() {
+        return Edu;
+    }
+
+    public String getAofI() {
+        return AofI;
+    }
+
+    public void setAofI(String AofI) {
+        this.AofI = AofI;
+    }
+
+    public String getAim() {
+        return aim;
+    }
 
 }
